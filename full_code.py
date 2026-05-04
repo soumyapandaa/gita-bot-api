b2127c1abb4d50638985731859cebb195801240f
soumyapandaa <soumyaranjanpanda985@gmail.com>
2026-05-04 23:12:09 +0530

    Fix: update text splitter import

--- full_code.py
+++ full_code.py
@@ -7,7 +7,7 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
 from datetime import datetime
 import time
